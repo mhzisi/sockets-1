cef42fb7290519d32fd5bebac1fd557d1b21d3da
syrine1001 <76111401+syrine1001@users.noreply.github.com>
2020-12-18 11:59:30 +0100

    Update client.java

--- client.java
+++ client.java
@@ -2,29 +2,22 @@ import java.io.*;
 import java.net.Socket;
 
 public class Client {
-
-    static final int port = 1200;
+     static final int port = 1200;
 
     public static void main(String[] args) throws Exception {
 
         System.out.println("connection request");
-
-        //Create a client socket
         Socket socket = new Socket("127.0.0.1", port);
         System.out.println("Connection established");
-
-        // create in / out
         BufferedReader inClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter outClient = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-
-        String str = "Hello World";
-        outClient.println(str); // send msg
-
-        //close in / out
+        
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+         String n = keyboard.readLine();
+        outClient.println(n);
+        
         inClient.close();
         outClient.close();
-
-        // close client socket
         socket.close();
     }
 }
